626fcfcfe1a4cc5dd8af0222c624203770813885
peatroot <garethpeat@gmail.com>
2018-07-23 10:57:41 +0100

    Add graphql parsing support

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireInlineImportGraphqlAst = require('react-app-rewire-inline-import-graphql-ast');
+
+module.exports = function override(config, env) {
+    // needed to parse graphql files
+    config = rewireInlineImportGraphqlAst(config, env);
+    return config;
+};
