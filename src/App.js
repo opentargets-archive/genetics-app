f27286d411ed69767af01ecdd72f47734feb3705
peatroot <garethpeat@gmail.com>
2018-07-26 11:43:59 +0100

    Tidy App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
-import { TestComponent } from 'ot-ui';
 
 import HomePage from './pages/HomePage';
 import StudyPage from './pages/StudyPage';
@@ -25,49 +21,6 @@ const App = () => (
             <Route path="/regional/:studyId/:variantId" component={RegionalPage} />
         </React.Fragment>
     </Router>
-)
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <TestComponent name='World' />
-
-//         <Query
-//                 query={gql`
-//                   {
-//                     pheWAS(variantId: "1_1002003_A_G") {
-//                       associations {
-//                         studyId
-//                         pmId
-//                         traitReported
-//                         n
-//                         nCases
-//                         eaf
-//                         beta
-//                         se
-//                         pval
-//                       }
-//                     }
-//                   }
-//                 `}
-//             >
-//                 {({ loading, error, data }) => {
-//                   console.log('data', data)
-//                   return null
-//                 }}
-//         </Query>
-
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
+);
 
 export default App;
