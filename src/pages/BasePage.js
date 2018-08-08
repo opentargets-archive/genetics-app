55371c77f4afb4d9e97773250fdbd5c25cb530cc
peatroot <garethpeat@gmail.com>
2018-08-08 10:15:53 +0100

    Simplify BasePage usage of header and footer

--- src/pages/BasePage.js
+++ src/pages/BasePage.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
 
 import { Page, NavBar, Footer } from 'ot-ui';
 
-const BasePage = ({ children }) => {
-    const header = (
-        <Switch>
-            <Route exact path="/" component={null} />
-            <Route path="/*" render={() => <NavBar name="Genetics" />} />
-        </Switch>
-    );
-    const footer = (
-        <Switch>
-            <Route exact path="/" component={null} />
-            <Route path="/*" render={() => <Footer style={{ bottom: 0 }} />} />
-        </Switch>
-    );
-    return (
-        <Page header={header} footer={footer}>
-            {children}
-        </Page>
-    );
-};
+const BasePage = ({ children }) => (
+  <Page
+    header={<NavBar name="Genetics" />}
+    footer={<Footer style={{ bottom: 0 }} />}
+  >
+    {children}
+  </Page>
+);
 
 export default BasePage;
