9600c813d8000b97d5cf39ba4623dd7e1736bff8
Gareth Peat <peatroot@users.noreply.github.com>
2018-08-14 09:41:38 +0100

    Update footer (#11)
    * Add fg-loadcss dep
    
    * Inject package and revision to footer

--- src/pages/BasePage.js
+++ src/pages/BasePage.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import pkg from '../../package.json';
 
 import { Page, NavBar, Footer } from 'ot-ui';
 
 const BasePage = ({ children }) => (
   <Page
     header={<NavBar name="Genetics" />}
-    footer={<Footer style={{ bottom: 0 }} />}
+    footer={
+      <Footer
+        style={{ bottom: 0 }}
+        version={pkg.version}
+        commitHash={
+          process.env.REACT_APP_REVISION
+            ? process.env.REACT_APP_REVISION
+            : '2222ccc'
+        }
+        githubUrl="https://github.com/opentargets/genetics-app"
+      />
+    }
   >
     {children}
   </Page>
