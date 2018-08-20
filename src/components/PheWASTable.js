ea1bfb8c1029a1659471dac4780343c6c6a127bb
Alfredo Miranda <alfredo@miranda.io>
2018-08-20 15:41:44 +0100

    Add sorting to tables

--- src/components/PheWASTable.js
+++ src/components/PheWASTable.js
@@ -4,33 +4,40 @@ import { OtTable, commaSeparate } from 'ot-ui';
 
 const tableColumns = [
   {
+    id: 'studyId',
     label: 'studyId',
     renderCell: rowData => (
       <Link to={`/study/${rowData.studyId}`}>{rowData.studyId}</Link>
     ),
   },
   {
+    id: 'traitReported',
     label: 'traitReported',
     key: 'traitReported',
   },
   {
+    id: 'pval',
     label: 'pval',
     renderCell: rowData => rowData.pval.toPrecision(3),
   },
   {
+    id: 'nCases',
     label: 'nCases',
     renderCell: rowData => commaSeparate(rowData.nCases),
   },
   {
+    id: 'nTotal',
     label: 'nTotal',
     renderCell: rowData => commaSeparate(rowData.nTotal),
   },
   // TODO: check status of traitCode with Miguel - should we expose?
   // {
+  //   id: 'traitCode',
   //   label: 'traitCode',
   //   key: 'traitCode',
   // },
   {
+    id: 'locusView',
     label: 'Locus View',
     renderCell: () => {
       return <Link to="/locus">Gecko Plot</Link>;
@@ -39,7 +46,14 @@ const tableColumns = [
 ];
 
 function PheWASTable({ associations }) {
-  return <OtTable columns={tableColumns} data={associations} />;
+  return (
+    <OtTable
+      columns={tableColumns}
+      data={associations}
+      sortBy="studyId"
+      order="desc"
+    />
+  );
 }
 
 export default PheWASTable;
--- src/pages/StudyPage.js
+++ src/pages/StudyPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
