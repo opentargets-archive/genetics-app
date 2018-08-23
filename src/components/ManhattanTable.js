7089b328061feb48d1789b8ee1ae8151697355a7
Gareth Peat <peatroot@users.noreply.github.com>
2018-08-23 12:35:26 +0100

    Updates to Study page (#27)
    * Update column names
    
    * Add tooltip text for credible and ld set sizes
    
    * Default sort by pval asc
    
    * Add study publication info above manhattan plot

--- src/components/ManhattanTable.js
+++ src/components/ManhattanTable.js
@@ -5,7 +5,7 @@ import { OtTable, commaSeparate } from 'ot-ui';
 export const tableColumns = [
   {
     id: 'indexVariantId',
-    label: 'indexVariantId',
+    label: 'Lead Variant',
     renderCell: rowData => (
       <Link to={`/variant/${rowData.indexVariantId}`}>
         {rowData.indexVariantId}
@@ -14,33 +14,31 @@ export const tableColumns = [
   },
   {
     id: 'indexVariantRsId',
-    label: 'indexVariantRsId',
+    label: 'rsID',
   },
   {
     id: 'pval',
-    label: 'pval',
+    label: 'P-value',
     renderCell: rowData => rowData.pval.toPrecision(3),
   },
   {
     id: 'credibleSetSize',
-    label: 'credibleSetSize',
+    label: 'Credible Set Size',
+    tooltip: 'Number of variants in 95% credible set at this locus',
     renderCell: rowData => commaSeparate(rowData.credibleSetSize),
   },
   {
     id: 'ldSetSize',
-    label: 'ldSetSize',
+    label: 'LD Set Size',
+    tooltip:
+      'Number of variants that are in LD (R2 >= 0.7) with this lead variant',
     renderCell: rowData => commaSeparate(rowData.ldSetSize),
   },
 ];
 
 function ManhattanTable({ data }) {
   return (
-    <OtTable
-      columns={tableColumns}
-      data={data}
-      sortBy="indexVariantId"
-      order="desc"
-    />
+    <OtTable columns={tableColumns} data={data} sortBy="pval" order="asc" />
   );
 }
 
