8c3f0bff6bacc06d7048e25bac7b9b1fb8873348
peatroot <garethpeat@gmail.com>
2018-08-28 14:16:10 +0100

    Add mock data for genes, tag variants and index variants (CDK6 locus)

---
+++ src/graphql-mocking/locusIndexVariants.js
@@ -0,0 +1,292 @@
+const indexVariants = [
+  {
+    id: 'rs10225965',
+    position: 92644264,
+  },
+  {
+    id: 'rs10260824',
+    position: 94008313,
+  },
+  {
+    id: 'rs10262915',
+    position: 94371161,
+  },
+  {
+    id: 'rs10266254',
+    position: 90134550,
+  },
+  {
+    id: 'rs10269774',
+    position: 92624658,
+  },
+  {
+    id: 'rs10272859',
+    position: 90689160,
+  },
+  {
+    id: 'rs10487112',
+    position: 90444092,
+  },
+  {
+    id: 'rs10487245',
+    position: 94084209,
+  },
+  {
+    id: 'rs10644111',
+    position: 92022865,
+  },
+  {
+    id: 'rs10953024',
+    position: 90874752,
+  },
+  {
+    id: 'rs112724722',
+    position: 93917074,
+  },
+  {
+    id: 'rs1133906',
+    position: 93135669,
+  },
+  {
+    id: 'rs113692724',
+    position: 90621328,
+  },
+  {
+    id: 'rs11428598',
+    position: 93228573,
+  },
+  {
+    id: 'rs115744676',
+    position: 94432092,
+  },
+  {
+    id: 'rs11975386',
+    position: 94075721,
+  },
+  {
+    id: 'rs12534506',
+    position: 93033013,
+  },
+  {
+    id: 'rs12671937',
+    position: 93025051,
+  },
+  {
+    id: 'rs12704645',
+    position: 92993336,
+  },
+  {
+    id: 'rs13221576',
+    position: 94377682,
+  },
+  {
+    id: 'rs13229255',
+    position: 92468297,
+  },
+  {
+    id: 'rs1429524',
+    position: 91314953,
+  },
+  {
+    id: 'rs144974794',
+    position: 93345350,
+  },
+  {
+    id: 'rs1476442',
+    position: 91643029,
+  },
+  {
+    id: 'rs147692810',
+    position: 89924805,
+  },
+  {
+    id: 'rs1548457',
+    position: 93563398,
+  },
+  {
+    id: 'rs17268829',
+    position: 94484487,
+  },
+  {
+    id: 'rs180238',
+    position: 93921135,
+  },
+  {
+    id: 'rs180239',
+    position: 93921103,
+  },
+  {
+    id: 'rs180242',
+    position: 93920284,
+  },
+  {
+    id: 'rs180273',
+    position: 93908983,
+  },
+  {
+    id: 'rs183110392',
+    position: 94423978,
+  },
+  {
+    id: 'rs189936349',
+    position: 94371162,
+  },
+  {
+    id: 'rs191698899',
+    position: 94386418,
+  },
+  {
+    id: 'rs2040494',
+    position: 92627591,
+  },
+  {
+    id: 'rs2110483',
+    position: 94250781,
+  },
+  {
+    id: 'rs2229188',
+    position: 92134309,
+  },
+  {
+    id: 'rs2282978',
+    position: 92635096,
+  },
+  {
+    id: 'rs2540552',
+    position: 91610982,
+  },
+  {
+    id: 'rs2888830',
+    position: 91286039,
+  },
+  {
+    id: 'rs34592535',
+    position: 94250134,
+  },
+  {
+    id: 'rs35417517',
+    position: 92143417,
+  },
+  {
+    id: 'rs35522438',
+    position: 92243559,
+  },
+  {
+    id: 'rs369328859',
+    position: 92344194,
+  },
+  {
+    id: 'rs3736638',
+    position: 94417954,
+  },
+  {
+    id: 'rs3808274',
+    position: 90986658,
+  },
+  {
+    id: 'rs42033',
+    position: 92608219,
+  },
+  {
+    id: 'rs42039',
+    position: 92615108,
+  },
+  {
+    id: 'rs42041',
+    position: 92617430,
+  },
+  {
+    id: 'rs42044',
+    position: 92620826,
+  },
+  {
+    id: 'rs42235',
+    position: 92618762,
+  },
+  {
+    id: 'rs4262',
+    position: 93922116,
+  },
+  {
+    id: 'rs42648',
+    position: 90348446,
+  },
+  {
+    id: 'rs4272',
+    position: 92607515,
+  },
+  {
+    id: 'rs441051',
+    position: 94424688,
+  },
+  {
+    id: 'rs445',
+    position: 92779056,
+  },
+  {
+    id: 'rs4729127',
+    position: 94379223,
+  },
+  {
+    id: 'rs6465411',
+    position: 94373948,
+  },
+  {
+    id: 'rs6964587',
+    position: 92001306,
+  },
+  {
+    id: 'rs73215715',
+    position: 89999321,
+  },
+  {
+    id: 'rs73407739',
+    position: 92861969,
+  },
+  {
+    id: 'rs73708431',
+    position: 91489997,
+  },
+  {
+    id: 'rs739385',
+    position: 93053295,
+  },
+  {
+    id: 'rs7780752',
+    position: 93612328,
+  },
+  {
+    id: 'rs7786765',
+    position: 93570616,
+  },
+  {
+    id: 'rs7792596',
+    position: 94380322,
+  },
+  {
+    id: 'rs7798970',
+    position: 90006910,
+  },
+  {
+    id: 'rs8',
+    position: 92779015,
+  },
+  {
+    id: 'rs813379',
+    position: 91152672,
+  },
+  {
+    id: 'rs8179',
+    position: 92606850,
+  },
+  {
+    id: 'rs9641123',
+    position: 93568420,
+  },
+  {
+    id: 'rs9918668',
+    position: 94062432,
+  },
+];
+
+export default indexVariants;
