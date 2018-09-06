263e056f83e4647e45fbb1b10ba4a446dc68f74e
Gareth Peat <peatroot@users.noreply.github.com>
2018-09-06 10:38:32 +0100

    Add search to navbar (#34)

--- src/pages/BasePage.js
+++ src/pages/BasePage.js
@@ -3,10 +3,11 @@ import { Helmet } from 'react-helmet';
 import { Page, NavBar, Footer } from 'ot-ui';
 
 import pkg from '../../package.json';
+import Search from '../components/Search';
 
 const BasePage = ({ children }) => (
   <Page
-    header={<NavBar name="Genetics" />}
+    header={<NavBar name="Genetics" search={<Search />} />}
     footer={
       <Footer
         style={{ bottom: 0 }}
