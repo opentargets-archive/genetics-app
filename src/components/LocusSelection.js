d206a91d5d52f2b9e9f3b4b8972f842b7c727bf6
peatroot <garethpeat@gmail.com>
2018-09-13 16:43:14 +0100

    Disable boolean expressions labels

--- src/components/LocusSelection.js
+++ src/components/LocusSelection.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Chip } from 'ot-ui';
+import { Chip, Text } from 'ot-ui';
+
+// const AND = 'AND';
+// const OR = 'OR';
 
 const LocusSelection = ({
   selectedGenes,
@@ -12,41 +15,56 @@ const LocusSelection = ({
   handleDeleteStudy,
 }) => (
   <div>
-    {selectedGenes
-      ? selectedGenes.map(d => (
-          <Chip key={d} label={d} type="gene" onDelete={handleDeleteGene(d)} />
-        ))
-      : null}
-    {selectedTagVariants
-      ? selectedTagVariants.map(d => (
-          <Chip
-            key={d}
-            label={d}
-            type="tagVariant"
-            onDelete={handleDeleteTagVariant(d)}
-          />
-        ))
-      : null}
-    {selectedIndexVariants
-      ? selectedIndexVariants.map(d => (
-          <Chip
-            key={d}
-            label={d}
-            type="indexVariant"
-            onDelete={handleDeleteIndexVariant(d)}
-          />
-        ))
-      : null}
-    {selectedStudies
-      ? selectedStudies.map(d => (
-          <Chip
-            key={d}
-            label={d}
-            type="study"
-            onDelete={handleDeleteStudy(d)}
-          />
-        ))
-      : null}
+    <Text>
+      {selectedGenes
+        ? selectedGenes.map(d => (
+            <Chip
+              key={d}
+              label={d}
+              type="gene"
+              onDelete={handleDeleteGene(d)}
+            />
+          ))
+        : null}
+      {/* {selectedGenes &&
+      (selectedTagVariants || selectedIndexVariants || selectedStudies)
+        ? AND
+        : null} */}
+      {selectedTagVariants
+        ? selectedTagVariants.map(d => (
+            <Chip
+              key={d}
+              label={d}
+              type="tagVariant"
+              onDelete={handleDeleteTagVariant(d)}
+            />
+          ))
+        : null}
+      {/* {selectedTagVariants && (selectedIndexVariants || selectedStudies)
+        ? AND
+        : null} */}
+      {selectedIndexVariants
+        ? selectedIndexVariants.map(d => (
+            <Chip
+              key={d}
+              label={d}
+              type="indexVariant"
+              onDelete={handleDeleteIndexVariant(d)}
+            />
+          ))
+        : null}
+      {/* {selectedIndexVariants && selectedStudies ? AND : null} */}
+      {selectedStudies
+        ? selectedStudies.map(d => (
+            <Chip
+              key={d}
+              label={d}
+              type="study"
+              onDelete={handleDeleteStudy(d)}
+            />
+          ))
+        : null}
+    </Text>
   </div>
 );
 
