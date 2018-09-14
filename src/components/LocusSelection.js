4f371810fe6d6506607c5ee05e5e28ee52ce2634
peatroot <garethpeat@gmail.com>
2018-09-14 11:19:22 +0100

    Show author and date in study chip

--- src/components/LocusSelection.js
+++ src/components/LocusSelection.js
@@ -19,33 +19,22 @@ const LocusSelection = ({
   return (
     <div>
       <Text>
-        {selectedGenes
-          ? selectedGenes.map(d => (
+        {selectedStudies
+          ? selectedStudies.map(d => (
               <Chip
                 key={d}
-                label={geneDict[d].symbol}
-                type="gene"
-                onDelete={handleDeleteGene(d)}
+                label={`${studyDict[d].traitReported}${
+                  studyDict[d].pubAuthor && studyDict[d].pubDate
+                    ? ` (${studyDict[d].pubAuthor} ${new Date(
+                        studyDict[d].pubDate
+                      ).getFullYear()})`
+                    : null
+                }`}
+                type="study"
+                onDelete={handleDeleteStudy(d)}
               />
             ))
           : null}
-        {/* {selectedGenes &&
-      (selectedTagVariants || selectedIndexVariants || selectedStudies)
-        ? AND
-        : null} */}
-        {selectedTagVariants
-          ? selectedTagVariants.map(d => (
-              <Chip
-                key={d}
-                label={d}
-                type="tagVariant"
-                onDelete={handleDeleteTagVariant(d)}
-              />
-            ))
-          : null}
-        {/* {selectedTagVariants && (selectedIndexVariants || selectedStudies)
-        ? AND
-        : null} */}
         {selectedIndexVariants
           ? selectedIndexVariants.map(d => (
               <Chip
@@ -56,14 +45,23 @@ const LocusSelection = ({
               />
             ))
           : null}
-        {/* {selectedIndexVariants && selectedStudies ? AND : null} */}
-        {selectedStudies
-          ? selectedStudies.map(d => (
+        {selectedTagVariants
+          ? selectedTagVariants.map(d => (
               <Chip
                 key={d}
-                label={studyDict[d].traitReported}
-                type="study"
-                onDelete={handleDeleteStudy(d)}
+                label={d}
+                type="tagVariant"
+                onDelete={handleDeleteTagVariant(d)}
+              />
+            ))
+          : null}
+        {selectedGenes
+          ? selectedGenes.map(d => (
+              <Chip
+                key={d}
+                label={geneDict[d].symbol}
+                type="gene"
+                onDelete={handleDeleteGene(d)}
               />
             ))
           : null}
