a49dd1ed20b7c44b8e809488422c2f62159540da
peatroot <garethpeat@gmail.com>
2018-09-17 12:35:00 +0100

    Add compare studies button to study page

--- src/pages/StudyPage.js
+++ src/pages/StudyPage.js
@@ -1,9 +1,16 @@
 import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
-import { Query } from 'react-apollo';
+import { Query, withApollo } from 'react-apollo';
 import gql from 'graphql-tag';
+import queryString from 'query-string';
 
-import { PageTitle, SubHeading, DownloadSVGPlot, SectionHeading } from 'ot-ui';
+import {
+  PageTitle,
+  SubHeading,
+  DownloadSVGPlot,
+  SectionHeading,
+  Button,
+} from 'ot-ui';
 import { Manhattan } from 'ot-charts';
 
 import BasePage from './BasePage';
@@ -75,88 +82,121 @@ const manhattanQuery = gql`
   }
 `;
 
-const StudyPage = ({ match }) => {
-  let manhattanPlot = React.createRef();
-  const { studyId } = match.params;
-  return (
-    <BasePage>
-      <ScrollToTop onRouteChange />
-      <Helmet>
-        <title>{studyId}</title>
-      </Helmet>
+const studySearchQuery = gql`
+  query StudySearchQuery($queryString: String!) {
+    search(queryString: $queryString) {
+      studies {
+        studyId
+      }
+    }
+  }
+`;
 
-      <Query
-        query={manhattanQuery}
-        variables={{ studyId }}
-        fetchPolicy="network-only"
-      >
-        {({ loading, error, data }) => {
-          const manhattan = hasAssociations(data)
-            ? transformAssociations(data)
-            : { associations: [] };
-          return (
-            <Fragment>
-              {hasStudyInfo(data) ? (
-                <Fragment>
-                  <PageTitle>{data.studyInfo.traitReported}</PageTitle>
-                  <SubHeading>
-                    {`${data.studyInfo.pubAuthor} et al (${new Date(
-                      data.studyInfo.pubDate
-                    ).getFullYear()}) `}
-                    <em>{`${data.studyInfo.pubJournal} `}</em>
-                    <a
-                      href={`http://europepmc.org/abstract/med/${
-                        data.studyInfo.pmid
-                      }`}
-                      target="_blank"
-                      rel="noopener noreferrer"
+class StudyPage extends React.Component {
+  handleClickCompareStudies = traitReported => () => {
+    // TODO: this will search for the first 10 related studies by traitReported,
+    //       but it would be much better to use the overlap table in the future
+    const { client, history } = this.props;
+    client
+      .query({
+        query: studySearchQuery,
+        variables: { queryString: traitReported },
+      })
+      .then(({ data }) => {
+        const studyIds = data.search.studies.map(d => d.studyId);
+        history.push(`/studies?${queryString.stringify({ studyIds })}`);
+      });
+  };
+  render() {
+    const { studyId } = this.props.match.params;
+    let manhattanPlot = React.createRef();
+    return (
+      <BasePage>
+        <ScrollToTop onRouteChange />
+        <Helmet>
+          <title>{studyId}</title>
+        </Helmet>
+
+        <Query
+          query={manhattanQuery}
+          variables={{ studyId }}
+          fetchPolicy="network-only"
+        >
+          {({ loading, error, data }) => {
+            const manhattan = hasAssociations(data)
+              ? transformAssociations(data)
+              : { associations: [] };
+            return (
+              <Fragment>
+                {hasStudyInfo(data) ? (
+                  <Fragment>
+                    <PageTitle>{data.studyInfo.traitReported}</PageTitle>
+                    <SubHeading>
+                      {`${data.studyInfo.pubAuthor} et al (${new Date(
+                        data.studyInfo.pubDate
+                      ).getFullYear()}) `}
+                      <em>{`${data.studyInfo.pubJournal} `}</em>
+                      <a
+                        href={`http://europepmc.org/abstract/med/${
+                          data.studyInfo.pmid
+                        }`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {data.studyInfo.pmid}
+                      </a>
+                    </SubHeading>
+                    <Button
+                      onClick={this.handleClickCompareStudies(
+                        data.studyInfo.traitReported
+                      )}
                     >
-                      {data.studyInfo.pmid}
-                    </a>
-                  </SubHeading>
-                </Fragment>
-              ) : null}
-              {hasAssociations(data) ? (
-                <Fragment>
-                  <SectionHeading
-                    heading="Independently-associated loci"
-                    subheading={`Found ${significantLoci(
-                      data
-                    )} loci with genome-wide
-                  significance (p-value < 5e-8)`}
-                    entities={[
-                      {
-                        type: 'study',
-                        fixed: true,
-                      },
-                      {
-                        type: 'indexVariant',
-                        fixed: false,
-                      },
-                    ]}
-                  />
-
-                  <DownloadSVGPlot
-                    svgContainer={manhattanPlot}
-                    filenameStem={`${studyId}-independently-associated-loci`}
-                  >
-                    <ManhattanWithTooltip
-                      data={manhattan}
-                      ref={manhattanPlot}
+                      Compare to related studies
+                    </Button>
+                  </Fragment>
+                ) : null}
+                {hasAssociations(data) ? (
+                  <Fragment>
+                    <SectionHeading
+                      heading="Independently-associated loci"
+                      subheading={`Found ${significantLoci(
+                        data
+                      )} loci with genome-wide
+                    significance (p-value < 5e-8)`}
+                      entities={[
+                        {
+                          type: 'study',
+                          fixed: true,
+                        },
+                        {
+                          type: 'indexVariant',
+                          fixed: false,
+                        },
+                      ]}
                     />
-                  </DownloadSVGPlot>
-                  <ManhattanTable
-                    data={manhattan.associations}
-                    filenameStem={`${studyId}-independently-associated-loci`}
-                  />
-                </Fragment>
-              ) : null}
-            </Fragment>
-          );
-        }}
-      </Query>
-    </BasePage>
-  );
-};
 
-export default StudyPage;
+                    <DownloadSVGPlot
+                      svgContainer={manhattanPlot}
+                      filenameStem={`${studyId}-independently-associated-loci`}
+                    >
+                      <ManhattanWithTooltip
+                        data={manhattan}
+                        ref={manhattanPlot}
+                      />
+                    </DownloadSVGPlot>
+                    <ManhattanTable
+                      data={manhattan.associations}
+                      filenameStem={`${studyId}-independently-associated-loci`}
+                    />
+                  </Fragment>
+                ) : null}
+              </Fragment>
+            );
+          }}
+        </Query>
+      </BasePage>
+    );
+  }
+}
+
+export default withApollo(StudyPage);
