02c4a04ad35dc1a56ec44d44293c82c243e0d389
peatroot <garethpeat@gmail.com>
2018-09-18 11:34:13 +0100

    Update GenePage to have link to gecko plot

--- src/pages/GenePage.js
+++ src/pages/GenePage.js
@@ -1,62 +1,67 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import { PageTitle, Heading, SubHeading } from 'ot-ui';
 
 import BasePage from './BasePage';
+import LocusLink from '../components/LocusLink';
 
-const GenePage = ({ match }) => (
-  <BasePage>
-    <Helmet>
-      <title>{match.params.geneId}</title>
-    </Helmet>
-    <PageTitle>{`Gene ${match.params.geneId}`}</PageTitle>
-    <hr />
-    <Heading>Associated variants</Heading>
-    <SubHeading>
-      Which variants are functionally linked to this gene?
-    </SubHeading>
-    <table>
-      <thead>
-        <tr>
-          <td>variant</td>
-          <td>rsid</td>
-          <td>g2v score</td>
-          <td>g2v evidence</td>
-          <td />
-          <td>...more columns to come</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <Link to="/variant/1_100314838_C_T">1_100314838_C_T</Link>
-          </td>
-          <td>rs3753486</td>
-          <td>0.8</td>
-          <td>GTEx</td>
-          <td>
-            <Link to="/locus">
-              <button>Gecko Plot</button>
-            </Link>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Link to="/variant/2_107731839_A_G">2_107731839_A_G</Link>
-          </td>
-          <td>rs3753487</td>
-          <td>0.92</td>
-          <td>VEP</td>
-          <td>
-            <Link to="/locus">
-              <button>Gecko Plot</button>
-            </Link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </BasePage>
-);
+const SEARCH_QUERY = gql`
+  query SearchQuery($queryString: String) {
+    search(queryString: $queryString) {
+      genes {
+        id
+        symbol
+        chromosome
+        start
+        end
+      }
+    }
+  }
+`;
+
+const GenePage = ({ match }) => {
+  const { geneId } = match.params;
+  return (
+    <BasePage>
+      <Query query={SEARCH_QUERY} variables={{ queryString: geneId }}>
+        {({ loading, error, data }) => {
+          if (loading) {
+            return <span>Fetching gene location and redirecting...</span>;
+          } else if (
+            data &&
+            data.search &&
+            data.search.genes &&
+            data.search.genes.length === 1
+          ) {
+            const { chromosome, start, end, symbol } = data.search.genes[0];
+            const position = Math.floor((start + end) / 2);
+            return (
+              <React.Fragment>
+                <Helmet>
+                  <title>{symbol}</title>
+                </Helmet>
+                <PageTitle>{symbol}</PageTitle>
+                <SubHeading>
+                  <LocusLink
+                    chromosome={chromosome}
+                    position={position}
+                    selectedGenes={[geneId]}
+                  >
+                    View locus
+                  </LocusLink>
+                </SubHeading>
+              </React.Fragment>
+            );
+          } else {
+            return <span>Could not find gene.</span>;
+          }
+        }}
+      </Query>
+    </BasePage>
+  );
+};
 
 export default GenePage;
