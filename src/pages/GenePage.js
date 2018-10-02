ba0df015450089e9e51e0fd010f1181c958d35e0
peatroot <garethpeat@gmail.com>
2018-10-02 11:10:30 +0100

    Add loading and error states to GenePage

--- src/pages/GenePage.js
+++ src/pages/GenePage.js
@@ -35,101 +35,122 @@ const SEARCH_QUERY = gql`
   }
 `;
 
+function hasGeneData(data, geneId) {
+  return (
+    data &&
+    data.search &&
+    data.search.genes &&
+    data.search.genes.length === 1 &&
+    data.search.genes[0].id === geneId
+  );
+}
+
+function geneData(data) {
+  return data.search.genes[0];
+}
+
+function hasAssociatedStudies(data) {
+  return data && data.studiesForGene;
+}
+
 const GenePage = ({ match }) => {
   const { geneId } = match.params;
   return (
     <BasePage>
       <Query query={SEARCH_QUERY} variables={{ geneId }}>
         {({ loading, error, data }) => {
-          if (loading) {
-            return <span>Fetching gene location and redirecting...</span>;
-          } else if (
-            data &&
-            data.search &&
-            data.search.genes &&
-            data.search.genes.length === 1 &&
-            data.studiesForGene
-          ) {
-            const { chromosome, start, end, symbol } = data.search.genes[0];
-            const position = Math.floor((start + end) / 2);
-            return (
-              <React.Fragment>
-                <Helmet>
-                  <title>{symbol}</title>
-                </Helmet>
-                <PageTitle>{symbol}</PageTitle>
-                <SubHeading
-                  left={`${chromosome}:${commaSeparate(start)}-${commaSeparate(
-                    end
-                  )} `}
-                />
+          const isValidGene = hasGeneData(data, geneId);
+          const gene = isValidGene ? geneData(data) : {};
+          const associatedStudies =
+            isValidGene && hasAssociatedStudies(data)
+              ? data.studiesForGene.map(d => d.study)
+              : [];
 
-                <SubHeading
-                  left={
+          const { chromosome, start, end, symbol } = gene;
+          return (
+            <React.Fragment>
+              <Helmet>
+                <title>{symbol}</title>
+              </Helmet>
+              <PageTitle>
+                {symbol} <span style={{ fontSize: '0.8rem' }}>{geneId}</span>
+              </PageTitle>
+              <SubHeading
+                left={
+                  isValidGene
+                    ? `${chromosome}:${commaSeparate(start)}-${commaSeparate(
+                        end
+                      )} `
+                    : null
+                }
+              />
+              <SubHeading
+                left={
+                  isValidGene ? (
                     <LocusLink
                       chromosome={chromosome}
-                      position={position}
+                      position={Math.round((start + end) / 2)}
                       selectedGenes={[geneId]}
                     >
                       View locus
                     </LocusLink>
-                  }
-                />
-                <SectionHeading heading="Useful links" />
-                <SubHeading
-                  left={
-                    <Fragment>
-                      <a
-                        href={`https://www.targetvalidation.org/target/${geneId}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Open Targets Platform
-                      </a>
-                      <br />
-                      <a
-                        href={`https://www.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=${geneId}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Ensembl
-                      </a>
-                      <br />
-                      <a
-                        href={`https://gtexportal.org/home/eqtls/byGene?geneId=${symbol}&tissueName=All`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        GTEx
-                      </a>
-                    </Fragment>
-                  }
-                />
-                <SectionHeading
-                  heading={`Associated studies`}
-                  subheading={`Which studies are associated with ${symbol}?`}
-                  entities={[
-                    {
-                      type: 'study',
-                      fixed: false,
-                    },
-                    {
-                      type: 'gene',
-                      fixed: true,
-                    },
-                  ]}
-                />
-                <AssociatedStudiesTable
-                  data={data.studiesForGene.map(d => d.study)}
-                  geneId={geneId}
-                  chromosome={chromosome}
-                  position={Math.round((start + end) / 2)}
-                />
-              </React.Fragment>
-            );
-          } else {
-            return <span>Could not find gene.</span>;
-          }
+                  ) : null
+                }
+              />
+              <SectionHeading heading="Useful links" />
+              <SubHeading
+                left={
+                  <Fragment>
+                    <a
+                      href={`https://www.targetvalidation.org/target/${geneId}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Open Targets Platform
+                    </a>
+                    <br />
+                    <a
+                      href={`https://www.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=${geneId}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Ensembl
+                    </a>
+                    <br />
+                    <a
+                      href={`https://gtexportal.org/home/eqtls/byGene?geneId=${symbol}&tissueName=All`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      GTEx
+                    </a>
+                  </Fragment>
+                }
+              />
+              <SectionHeading
+                heading={`Associated studies`}
+                subheading={`Which studies are associated with ${symbol}?`}
+                entities={[
+                  {
+                    type: 'study',
+                    fixed: false,
+                  },
+                  {
+                    type: 'gene',
+                    fixed: true,
+                  },
+                ]}
+              />
+              <AssociatedStudiesTable
+                loading={loading}
+                error={error}
+                data={associatedStudies}
+                geneId={geneId}
+                chromosome={chromosome}
+                position={Math.round((start + end) / 2)}
+              />
+            </React.Fragment>
+          );
         }}
       </Query>
     </BasePage>
