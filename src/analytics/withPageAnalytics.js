272bf35e47f0d00d258414564619a5521ebcac8f
peatroot <garethpeat@gmail.com>
2018-10-08 10:49:33 +0100

    Report duration of time on page

--- src/analytics/withPageAnalytics.js
+++ src/analytics/withPageAnalytics.js
@@ -3,6 +3,7 @@ import GoogleAnalytics from 'react-ga';
 
 import { ANALYTICS_TOKEN } from './constants';
 import useAnalytics from './useAnalytics';
+import reportAnalyticsEvent from './reportAnalyticsEvent';
 
 if (useAnalytics()) {
   GoogleAnalytics.initialize(ANALYTICS_TOKEN);
@@ -25,11 +26,25 @@ const withPageAnalytics = (WrappedComponent, options = {}) => {
   };
 
   const HOC = class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { loadedAt: new Date() };
+    }
     componentDidMount() {
       const page = this.props.location.pathname;
       trackPage(page);
     }
-
+    componentWillUnmount() {
+      const page = this.props.location.pathname;
+      const unmountedAt = new Date();
+      const duration = unmountedAt - this.state.loadedAt;
+      reportAnalyticsEvent({
+        category: 'page',
+        action: 'leave',
+        label: page,
+        value: duration,
+      });
+    }
     componentWillReceiveProps(nextProps) {
       const currentPage = this.props.location.pathname;
       const nextPage = nextProps.location.pathname;
