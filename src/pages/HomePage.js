643ec3f63394f0b896bc55ee0559605ce40a1545
Alfredo Miranda <alfredo@miranda.io>
2018-10-16 15:02:58 +0100

    First pass at homepage redesign

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
 
-import { Splash, HomeBox, Typography } from 'ot-ui';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import { Splash, HomeBox, Footer } from 'ot-ui';
 
 import Search from '../components/Search';
+import PortalFeaturesIcon from '../components/PortalFeaturesIcon';
 import reportAnalyticsEvent from '../analytics/reportAnalyticsEvent';
 
 const EXAMPLES = [
@@ -29,39 +34,74 @@ const clickExample = type => () => {
   });
 };
 
-const HomePage = () => (
-  <div>
+const styles = theme => {
+  return {
+    highlight: {
+      color: theme.palette.primary.main,
+      fontWeight: 'bold',
+    },
+    searchSection: {
+      position: 'relative',
+      height: '100vh',
+    },
+  };
+};
+
+const HomePage = ({ classes }) => (
+  <Fragment>
     <Helmet>
       <title>Open Targets Genetics</title>
     </Helmet>
-    <Splash />
-    <HomeBox name="Genetics">
-      <Search />
-      <Typography style={{ marginTop: '25px' }}>
-        Examples:
-        <br />
-        {EXAMPLES.map((d, i) => (
-          <a
-            key={i}
-            href={d.url}
-            style={{ marginRight: '15px' }}
-            onClick={clickExample(d.type)}
-          >
-            {d.label}
-          </a>
-        ))}
-      </Typography>
-      <Typography style={{ marginTop: '25px', textAlign: 'center' }}>
-        <a
-          href="http://eepurl.com/dHnchn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Subscribe to our newsletter
-        </a>
-      </Typography>
-    </HomeBox>
-  </div>
+    <Grid className={classes.searchSection} container justify="center">
+      <Splash />
+      <Grid item>hello</Grid>
+    </Grid>
+    <Grid container justify="center">
+      <Grid item md={8}>
+        <Grid container>
+          <Grid item md={12}>
+            <Typography variant="headline">
+              Welcome to Open Targets Genetics
+            </Typography>
+            <Typography paragraph>
+              Open Targets Genetics is the latest release from Open Targets, an
+              innovative, large-scale, multi-year, public-private partnership
+              that uses human genetics and genmics data for systematic drug
+              target identification and prioritisation.
+            </Typography>
+            <Typography paragraph>
+              The Portal offers three unique features to help you discover
+              associations between genes, variants, and traits:
+            </Typography>
+          </Grid>
+          <Grid item md={6}>
+            <Typography paragraph>
+              + Browse and rank gene and variant associations identified by our{' '}
+              <span className={classes.highlight}>
+                Gene2Variant (g2v) scoring
+              </span>{' '}
+              pipeline
+            </Typography>
+            <Typography paragraph>
+              + Uncover credible sets for variant and trait associations based
+              on our{' '}
+              <span className={classes.highlight}>fine mapping analyses</span>{' '}
+              pipeline
+            </Typography>
+            <Typography paragraph>
+              + Explore and compare studies from both UK Biobank and GWAS
+              Catalog using our{' '}
+              <span className={classes.highlight}>multi-trait comparison</span>{' '}
+              tool
+            </Typography>
+          </Grid>
+          <Grid item md={6}>
+            <PortalFeaturesIcon />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  </Fragment>
 );
 
-export default HomePage;
+export default withStyles(styles)(HomePage);
