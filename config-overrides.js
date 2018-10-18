7526297c30c45d16c44ad9bbd9b6a5abb1753c61
peatroot <garethpeat@gmail.com>
2018-10-18 16:52:24 +0100

    Update rewiring of gql files

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,9 @@
 const rewireInlineImportGraphqlAst = require('react-app-rewire-inline-import-graphql-ast');
+const rewireGraphqlTag = require('react-app-rewire-graphql-tag');
 
 module.exports = function override(config, env) {
   // needed to parse graphql files
   config = rewireInlineImportGraphqlAst(config, env);
+  config = rewireGraphqlTag(config, env);
   return config;
 };
