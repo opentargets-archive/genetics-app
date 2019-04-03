18cba5e7faf40bf93403bc38036056d0bd3c6e40
peatroot <garethpeat@gmail.com>
2019-04-03 14:54:49 +0100

    Use DataDownloader and OtTableRF for AssociatedStudiesTable

--- src/components/AssociatedStudiesTable.js
+++ src/components/AssociatedStudiesTable.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
-  OtTable,
+  DataDownloader,
+  OtTableRF,
   commaSeparate,
   significantFigures,
   Autocomplete,
@@ -12,6 +13,50 @@ import LocusLink from './LocusLink';
 import reportAnalyticsEvent from '../analytics/reportAnalyticsEvent';
 import PmidOrBiobankLink from './PmidOrBiobankLink';
 import generateComparator from '../utils/generateComparator';
+
+const getDownloadColumns = () => {
+  return [
+    { id: 'studyId', label: 'Study ID' },
+    { id: 'studyTrait', label: 'Reported Trait' },
+    { id: 'studyPmid', label: 'PMID' },
+    { id: 'studyAuthor', label: 'Author' },
+    { id: 'studyDate', label: 'Date' },
+    { id: 'nInitial', label: 'Study N Initial' },
+    { id: 'nReplication', label: 'Study N Replication' },
+    { id: 'nCases', label: 'Study N Cases' },
+    { id: 'indexVariantId', label: 'Index Variant ID' },
+    { id: 'indexVariantRsId', label: 'Index Variant RSID' },
+    { id: 'pval', label: 'P-Value' },
+    { id: 'beta', label: 'Beta' },
+    { id: 'betaCILower', label: 'Beta CI Lower' },
+    { id: 'betaCIUpper', label: 'Beta CI Upper' },
+    { id: 'oddsRatio', label: 'Odds Ratio' },
+    { id: 'oddsRatioCILower', label: 'Odds Ratio CI Lower' },
+    { id: 'oddsRatioCIUpper', label: 'Odds Ratio CI Upper' },
+  ];
+};
+
+const getDownloadRows = rows => {
+  return rows.map(row => ({
+    studyId: row.study.studyId,
+    studyTrait: row.study.traitReported,
+    studyPmid: row.study.pmid,
+    studyAuthor: row.study.pubAuthor,
+    studyDate: row.study.pubDate,
+    nInitial: row.study.nInitial,
+    nReplication: row.study.nReplication,
+    nCases: row.study.nCases,
+    indexVariantId: row.indexVariant.id,
+    indexVariantRsId: row.indexVariant.rsId,
+    pval: row.pval,
+    beta: row.beta,
+    betaCILower: row.betaCILower,
+    betaCIUpper: row.betaCIUpper,
+    oddsRatio: row.oddsRatio,
+    oddsRatioCILower: row.oddsRatioCILower,
+    oddsRatioCIUpper: row.oddsRatioCIUpper,
+  }));
+};
 
 const tableColumns = ({
   geneId,
@@ -180,40 +225,46 @@ const AssociatedStudiesTable = ({
   authorFilterOptions,
   authorFilterHandler,
 }) => (
-  <OtTable
-    loading={loading}
-    error={error}
-    filters
-    columns={tableColumns({
-      geneId,
-      chromosome,
-      position,
-      traitFilterValue,
-      traitFilterOptions,
-      traitFilterHandler,
-      authorFilterValue,
-      authorFilterOptions,
-      authorFilterHandler,
-    })}
-    data={data}
-    sortBy="study.nInitial"
-    order="desc"
-    downloadFileStem={filenameStem}
-    reportTableDownloadEvent={format => {
-      reportAnalyticsEvent({
-        category: 'table',
-        action: 'download',
-        label: `gene:associated-studies:${format}`,
-      });
-    }}
-    reportTableSortEvent={(sortBy, order) => {
-      reportAnalyticsEvent({
-        category: 'table',
-        action: 'sort-column',
-        label: `gene:associated-studies:${sortBy}(${order})`,
-      });
-    }}
-  />
+  <React.Fragment>
+    <DataDownloader
+      tableHeaders={getDownloadColumns()}
+      rows={getDownloadRows(data)}
+      fileStem={filenameStem}
+    />
+    <OtTableRF
+      loading={loading}
+      error={error}
+      filters
+      columns={tableColumns({
+        geneId,
+        chromosome,
+        position,
+        traitFilterValue,
+        traitFilterOptions,
+        traitFilterHandler,
+        authorFilterValue,
+        authorFilterOptions,
+        authorFilterHandler,
+      })}
+      data={data}
+      sortBy="study.nInitial"
+      order="desc"
+      reportTableDownloadEvent={format => {
+        reportAnalyticsEvent({
+          category: 'table',
+          action: 'download',
+          label: `gene:associated-studies:${format}`,
+        });
+      }}
+      reportTableSortEvent={(sortBy, order) => {
+        reportAnalyticsEvent({
+          category: 'table',
+          action: 'sort-column',
+          label: `gene:associated-studies:${sortBy}(${order})`,
+        });
+      }}
+    />
+  </React.Fragment>
 );
 
 export default AssociatedStudiesTable;
