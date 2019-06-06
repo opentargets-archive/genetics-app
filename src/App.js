a8da0f008c322dbe40d063abe34316988c934633
peatroot <garethpeat@gmail.com>
2019-06-06 11:28:39 +0100

    Remove LocusTraitPage

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import StudiesPage from './pages/StudiesPage';
 import GenePage from './pages/GenePage';
 import VariantPage from './pages/VariantPage';
 import LocusPage from './pages/LocusPage';
-import LocusTraitPage from './pages/LocusTraitPage';
 import StudyLocusPage from './pages/StudyLocusPage';
 import withPageAnalytics from './analytics/withPageAnalytics';
 
@@ -41,10 +40,6 @@ const App = () => (
         <Route
           path="/study-locus/:studyId/:indexVariantId"
           component={withPageAnalytics('study-locus', StudyLocusPage)}
-        />
-        <Route
-          path="/locus-trait/:studyId/:indexVariantId"
-          component={withPageAnalytics('study-locus', LocusTraitPage)}
         />
       </React.Fragment>
     </Router>
