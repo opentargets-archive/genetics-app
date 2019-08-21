5e24cb4ebdeb1f4b47d45f1e0f25c43975abf9e9
peatroot <garethpeat@gmail.com>
2019-08-21 11:18:39 +0100

    Add immunobase text and links

--- src/pages/ImmunobasePage.js
+++ src/pages/ImmunobasePage.js
@@ -1,51 +1,85 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+
+import { Link, Button } from 'ot-ui';
 
 import BasePage from './BasePage';
 
 const styles = theme => {
   return {
-    section: {
+    information: {
       height: '100%',
-      padding: theme.sectionPadding,
-    },
-    geneSymbol: {
-      display: 'inline-block',
-    },
-    locusLinkButton: {
-      width: '248px',
-      height: '60px',
-    },
-    locusIcon: {
-      fill: 'white',
-      width: '40px',
-      height: '40px',
-    },
-    link: {
-      textDecoration: 'none',
-    },
-    geneInfoItem: {
-      width: '20%',
-    },
-    platformLink: {
-      textAlign: 'center',
-      textDecoration: 'none',
-      '&:hover': {
-        textDecoration: 'underline',
-      },
-    },
-    iconLink: {
-      '&:hover': {
-        fill: theme.palette.primary.dark,
-      },
     },
   };
 };
 
 class ImmunobasePage extends React.Component {
   render() {
-    return <BasePage>hello</BasePage>;
+    const { classes } = this.props;
+    return (
+      <BasePage>
+        <Grid
+          className={classes.information}
+          container
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} sm={10} md={6}>
+            <Typography align="center">
+              <p>
+                ImmunoBase ownership has been transferred to{' '}
+                <Link external to="https://www.opentargets.org/">
+                  Open Targets
+                </Link>
+                .
+              </p>
+              <p>
+                Results for all publicly available GWAS, including ImmunoBase
+                studies, can be explored using{' '}
+                <Link to="/">Open Targets Genetics</Link>. You can search by{' '}
+                <Link to="/study/GCST005536">study</Link>,{' '}
+                <Link to="/variant/2_203874196_G_A">variant</Link> or{' '}
+                <Link to="/gene/ENSG00000162594">gene</Link> to find
+                trait-associated loci and prioritise genes using our{' '}
+                <Link
+                  external
+                  to="https://genetics-docs.opentargets.org/our-approach/data-pipeline"
+                >
+                  variant-to-gene
+                </Link>{' '}
+                and{' '}
+                <Link
+                  external
+                  to="https://genetics-docs.opentargets.org/our-approach/colocalisation-analysis"
+                >
+                  colocalisation
+                </Link>{' '}
+                pipelines.
+              </p>
+              <p>
+                Summary statistics and curated data previously hosted by
+                ImmunoBase can now be downloaded from the{' '}
+                <Link external to="https://www.ebi.ac.uk/gwas/">
+                  NHGRI-EBI GWAS Catalog
+                </Link>{' '}
+                (see table below for links).
+              </p>
+              <p>
+                <Button variant="outlined">
+                  <Link
+                    external
+                    to="https://docs.google.com/spreadsheets/d/1YYbxC1NhtbYuBYe2gYZNcxO0a0S4oTxHfoYtZrqKsrM/edit#gid=1589938306"
+                  >
+                    Table with links
+                  </Link>
+                </Button>
+              </p>
+            </Typography>
+          </Grid>
+        </Grid>
+      </BasePage>
+    );
   }
 }
 
