3d6927b75058a2fa1390897388f2aaea4e951b19
carcruz <me@carcruzcast.com>
2021-08-02 07:14:18 -0600

    refactor: App to apollo v3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { ApolloProvider as ApolloProviderR } from 'react-apollo';
 import { ApolloProvider } from '@apollo/client';
 import { OtUiThemeProvider } from 'ot-ui';
 
-import { client, clientsss } from './client';
+import client from './client';
 import HomePage from './pages/HomePage';
 import StudyPage from './pages/StudyPage';
 import StudiesPage from './pages/StudiesPage';
@@ -16,25 +15,23 @@ import ImmunobasePage from './pages/ImmunobasePage';
 
 const App = () => (
   <ApolloProvider client={client}>
-    <ApolloProviderR client={clientsss}>
-      <OtUiThemeProvider>
-        <Router>
-          <React.Fragment>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/study/:studyId" component={StudyPage} />
-            <Route path="/study-comparison/:studyId" component={StudiesPage} />
-            <Route path="/gene/:geneId" component={GenePage} />
-            <Route path="/variant/:variantId" component={VariantPage} />
-            <Route path="/locus" component={LocusPage} />
-            <Route
-              path="/study-locus/:studyId/:indexVariantId"
-              component={StudyLocusPage}
-            />
-            <Route path="/immunobase" component={ImmunobasePage} />
-          </React.Fragment>
-        </Router>
-      </OtUiThemeProvider>
-    </ApolloProviderR>
+    <OtUiThemeProvider>
+      <Router>
+        <React.Fragment>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/study/:studyId" component={StudyPage} />
+          <Route path="/study-comparison/:studyId" component={StudiesPage} />
+          <Route path="/gene/:geneId" component={GenePage} />
+          <Route path="/variant/:variantId" component={VariantPage} />
+          <Route path="/locus" component={LocusPage} />
+          <Route
+            path="/study-locus/:studyId/:indexVariantId"
+            component={StudyLocusPage}
+          />
+          <Route path="/immunobase" component={ImmunobasePage} />
+        </React.Fragment>
+      </Router>
+    </OtUiThemeProvider>
   </ApolloProvider>
 );
 
